5d92e8c8890e78d55c007f495e7407378ab8a374
SamuelSVD <svd.muelo1000+sourcetree@gmail.com>
2021-03-10 01:44:58 -0500

    Working on building UI for each instance

--- TrayDir/src/forms/MainForm.cs
+++ TrayDir/src/forms/MainForm.cs
@@ -24,27 +24,50 @@ namespace TrayDir
             trayInstance = pd.trayInstances[0];
             pd.Save();
             InitializeAllAssets();
+            InitializeInstanceTabs();
+        }
+        private void InitializeInstanceTabs()
+        {
+            for (int i = 0; i < pd.trayInstances.Count; i++)
+            {
+                TrayInstance instance = pd.trayInstances[i];
+                TabPage tp;
+                if (i == 0)
+                {
+                    tp = instanceTabs.TabPages[0];
+                    tp.Text = instance.instanceName;
+                }
+                else
+                {
+                    tp = new TabPage(instance.instanceName);
+                    instanceTabs.TabPages.Add(tp);
+                }
+                CreateViewFromInstance(instance, tp);
+            }
         }
         public static void Init()
         {
             form = new MainForm();
-
         }
         public void InitializePaths()
         {
-            foreach (TrayInstance instance in pd.trayInstances) {
+            foreach (TrayInstance instance in pd.trayInstances)
+            {
                 instance.setEventHandlers(ShowApp, HideApp, ExitApp);
             }
             if (trayInstance.settings.paths.Count == 0)
             {
                 trayInstance.settings.paths.Add(".");
-                AddPath(".");
+                ControlUtils.AddPath(DirectoriesGroupLayout, 0, ".", FileDialog, trayInstance);
+                DirCount++;
             }
             else
             {
-                foreach (string path in trayInstance.settings.paths)
+                for (int i = 0; i < trayInstance.settings.paths.Count; i++)
                 {
-                    AddPath(path);
+                    string path = trayInstance.settings.paths[i];
+                    ControlUtils.AddPath(DirectoriesGroupLayout, i, path, FileDialog, trayInstance);
+                    DirCount++;
                 }
             }
             if (trayInstance.settings.paths.Count == 1)
@@ -76,121 +99,11 @@ namespace TrayDir
                 pd.FormHidden();
             }
         }
-        private void AddPath(string text)
-        {
-            int n = DirCount;
-            // 
-            // textBox1
-            // 
-            TextBox textbox = new TextBox();
-            textbox.BorderStyle = BorderStyle.None;
-            textbox.Name = "textbox" + n.ToString();
-            textbox.Dock = DockStyle.Fill;
-            textbox.Margin = new Padding(10);
-            textbox.AutoSize = true;
-            textbox.ReadOnly = true;
-
-            if (AppUtils.PathIsDirectory(text))
-            {
-                textbox.Text = new DirectoryInfo(text).FullName;
-            }
-            else if (AppUtils.PathIsFile(text))
-            {
-                textbox.Text = Path.GetFullPath(text);
-            }
-            else
-            {
-                textbox.Text = text;
-            }
-            // panel
-            Panel panel = new Panel();
-            panel.AutoSize = true;
-            panel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            panel.Cursor = textbox.Cursor;
-            panel.BackColor = textbox.BackColor;
-            panel.BorderStyle = BorderStyle.FixedSingle;
-            panel.Dock = DockStyle.Fill;
-            panel.Margin = new Padding(2, 3, 2, 3);
-            panel.Name = "panel" + n.ToString();
-            panel.Padding = new Padding(0);
-
-            EventHandler textbox_select = new EventHandler(delegate (object obj, EventArgs args)
-            {
-                textbox.Select();
-            });
-            panel.Click += textbox_select;
-
-            panel.Controls.Add(textbox);
-
-            // 
-            // fileButton
-            // 
-            Button fileButton = new Button();
-            fileButton.AutoSize = true;
-            fileButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            fileButton.Name = "filebutton" + n.ToString();
-            fileButton.TabIndex = 1;
-            fileButton.Text = "File";
-            fileButton.UseVisualStyleBackColor = true;
-            fileButton.Dock = DockStyle.Fill;
-
-            EventHandler fileSelect = new EventHandler(delegate (object obj, EventArgs args)
-            {
-                FileDialog.DereferenceLinks = false;
-                FileDialog.InitialDirectory = textbox.Text;
-                DialogResult d = FileDialog.ShowDialog();
-                if (d == DialogResult.OK)
-                {
-                    textbox.Text = FileDialog.FileName;
-                    trayInstance.settings.paths[n] = textbox.Text;
-                    trayInstance.UpdateTrayMenu();
-                }
-            });
-
-            fileButton.Click += fileSelect;
-
-            // 
-            // folderButton
-            // 
-            Button folderButton = new Button();
-            folderButton.AutoSize = true;
-            folderButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            folderButton.Text = "Folder";
-            folderButton.UseVisualStyleBackColor = true;
-            folderButton.Dock = DockStyle.Fill;
-
-            EventHandler folderSelect = new EventHandler(delegate (object obj, EventArgs args)
-            {
-                FolderSelectDialog fs = new FolderSelectDialog();
-                fs.InitialDirectory = textbox.Text;
-                if (fs.ShowDialog())
-                {
-                    textbox.Text = fs.FileName;
-                    trayInstance.settings.paths[n] = textbox.Text;
-                    trayInstance.UpdateTrayMenu();
-                }
-            });
-
-            folderButton.Click += folderSelect;
-
-            DirectoriesGroupLayout.Controls.Add(panel, 0, DirCount);
-            DirectoriesGroupLayout.Controls.Add(fileButton, 1, DirCount);
-            DirectoriesGroupLayout.Controls.Add(folderButton, 2, DirCount);
-            DirCount += 1;
-            DirectoriesGroupLayout.RowCount = DirCount;
-            DirectoriesGroupLayout.Height = 0;
-            Height = 0;
-            foreach (RowStyle style in DirectoriesGroupLayout.RowStyles)
-            {
-                style.SizeType = SizeType.AutoSize;
-            }
-            trayInstance.UpdateTrayMenu();
-            RemoveButton.Enabled = DirCount > 1;
-        }
         private void button1_Click(object sender, EventArgs e)
         {
             trayInstance.AddPath(".");
-            AddPath(".");
+            ControlUtils.AddPath(DirectoriesGroupLayout, DirCount, ".", FileDialog, trayInstance);
+            DirCount++;
             Settings.Alter();
             CheckIsAltered();
         }
@@ -342,6 +255,81 @@ namespace TrayDir
             }
             PerformLayout();
             MaximizeBox = false;
+        }
+        public void CreateViewFromInstance(TrayInstance instance, TabPage tp)
+        {
+            TableLayoutPanel tlp = new TableLayoutPanel();
+            ControlUtils.ConfigureTableLayoutPanel(tlp);
+            tp.Controls.Add(tlp);
+
+            // Paths Group
+            GroupBox pathsgb = new GroupBox();
+            pathsgb.Text = "Paths";
+            ControlUtils.ConfigureGroupBox(pathsgb);
+            tlp.Controls.Add(pathsgb, 0, 0);
+
+            TableLayoutPanel pathstlp = new TableLayoutPanel();
+            ControlUtils.ConfigureTableLayoutPanel(pathstlp);
+            pathsgb.Controls.Add(pathstlp);
+
+            for (int i = 0; i < instance.settings.paths.Count; i++)
+            {
+                string path = instance.settings.paths[i];
+                ControlUtils.AddPath(pathstlp, i, path, FileDialog, instance);
+                //TODO Continue here
+                int j = 1 / (instance.settings.paths.Count - instance.settings.paths.Count);
+            }
+            // Options Group
+            GroupBox optionsgb = new GroupBox();
+            optionsgb.Text = "Tray Options";
+            ControlUtils.ConfigureGroupBox(optionsgb);
+            tlp.Controls.Add(optionsgb, 0, 1);
+
+            TableLayoutPanel optionstlp = new TableLayoutPanel();
+            ControlUtils.ConfigureTableLayoutPanel(optionstlp);
+            optionsgb.Controls.Add(optionstlp);
+
+            // Add options into table layout
+            EventHandler cbClick;
+            CheckBox cb1;
+            cb1 = ControlUtils.AddOption(optionstlp, 0, "Run As Admin", instance.settings.RunAsAdmin);
+            cbClick = new EventHandler(delegate (object obj, EventArgs args)
+            {
+                instance.settings.RunAsAdmin = cb1.Checked;
+                instance.UpdateTrayMenu();
+                pd.Save();
+            });
+            cb1.Click += cbClick;
+
+            CheckBox cb2;
+            cb2 = ControlUtils.AddOption(optionstlp, 1, "Show File Extensions", instance.settings.ShowFileExtensions);
+            cbClick = new EventHandler(delegate (object obj, EventArgs args)
+            {
+                instance.settings.ShowFileExtensions = cb2.Checked;
+                instance.UpdateTrayMenu();
+                pd.Save();
+            });
+            cb2.Click += cbClick;
+
+            CheckBox cb3;
+            cb3 = ControlUtils.AddOption(optionstlp, 2, "Explore Folders In TrayMenu", instance.settings.ExploreFoldersInTrayMenu);
+            cbClick = new EventHandler(delegate (object obj, EventArgs args)
+            {
+                instance.settings.ExploreFoldersInTrayMenu = cb3.Checked;
+                instance.UpdateTrayMenu();
+                pd.Save();
+            });
+            cb3.Click += cbClick;
+
+            CheckBox cb4;
+            cb4 = ControlUtils.AddOption(optionstlp, 3, "Expand First Path", instance.settings.ExpandFirstPath);
+            cbClick = new EventHandler(delegate (object obj, EventArgs args)
+            {
+                instance.settings.ExpandFirstPath = cb4.Checked;
+                instance.UpdateTrayMenu();
+                pd.Save();
+            });
+            cb4.Click += cbClick;
         }
     }
 }
--- TrayDir/src/forms/SettingsForm.cs
+++ TrayDir/src/forms/SettingsForm.cs
@@ -35,46 +35,6 @@ namespace TrayDir
             //    AddOption(option.name);
             //}
         }
-        private CheckBox AddOption(string name, bool boxChecked)
-        {
-            string text = AppUtils.SplitCamelCase(name);
-            Label label = new Label();
 
-            label.Anchor = ((AnchorStyles)((AnchorStyles.Left | AnchorStyles.Right)));
-            label.AutoSize = true;
-            label.Location = new System.Drawing.Point(10, 55);
-            label.Margin = new Padding(10, 5, 3, 5);
-            label.Name = name + "Label";
-            label.Size = new System.Drawing.Size(670, 25);
-            label.TabIndex = 2;
-            label.Text = text;
-
-            CheckBox checkbox = new CheckBox();
-            checkbox.Anchor = ((AnchorStyles)((AnchorStyles.Left | AnchorStyles.Right)));
-            checkbox.AutoSize = true;
-            checkbox.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            checkbox.Location = new System.Drawing.Point(688, 9);
-            checkbox.Name = name + "CheckBox";
-            checkbox.Size = new System.Drawing.Size(116, 27);
-            checkbox.TabIndex = 1;
-            checkbox.UseVisualStyleBackColor = true;
-            checkbox.Checked = boxChecked;
-
-            EventHandler folderSelect = new EventHandler(delegate (object obj, EventArgs args)
-            {
-                int i = 1 / (SettingsForm.form.OptionsGroupLayout.RowCount - SettingsForm.form.OptionsGroupLayout.RowCount);//Settings.setOption(name, checkbox.Checked);
-                MainForm.form.pd.UpdateAllMenus();
-            });
-
-            checkbox.Click += folderSelect;
-
-            int n = SettingsForm.form.OptionsGroupLayout.RowCount;
-            SettingsForm.form.OptionsGroupLayout.Controls.Add(label, 0, OptionCount);
-            SettingsForm.form.OptionsGroupLayout.Controls.Add(checkbox, 1, OptionCount);
-            OptionCount += 1;
-            SettingsForm.form.OptionsGroupLayout.RowCount = OptionCount;
-
-            return checkbox;
-        }
     }
 }
