bdad0496bbcb0dabe79d0497571210e8082b1ea3
SamuelSVD <svd.muelo1000+sourcetree@gmail.com>
2021-04-24 16:30:15 -0400

    Reorganizing + starting instance path refactoring

---
+++ TrayDir/src/models/TrayInstancePath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TrayDir
+{
+    public class TrayInstancePath
+    {
+        [XmlAttribute]
+        public string path;
+        [XmlAttribute]
+        public string alias;
+    }
+}
