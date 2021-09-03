f13207df3ab00a0f18eea83f84e86432d64510c2
SamuelSVD <svd.muelo1000+sourcetree@gmail.com>
2021-09-02 22:43:53 -0400

    Added check that TrayDir isn't already running

--- TrayDir/src/Program.cs
+++ TrayDir/src/Program.cs
@@ -12,18 +12,22 @@ namespace TrayDir
         [STAThread]
         static void Main()
         {
-            try
+            bool running = System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Length > 1;
+            if (!running || MessageBox.Show("Another process of TrayDir is already running. Do you want to start a new one?","Already running",MessageBoxButtons.OKCancel) == DialogResult.OK)
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                MainForm.Init();
-                SettingsForm.Init();
-                ProgramData.pd.initialized = true;
-                Application.Run(MainForm.form);
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show("Unhandled Fatal Exception: " + e.Message + "[" + e.StackTrace + "]");
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    MainForm.Init();
+                    SettingsForm.Init();
+                    ProgramData.pd.initialized = true;
+                    Application.Run(MainForm.form);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Unhandled Fatal Exception: " + e.Message + "[" + e.StackTrace + "]");
+                }
             }
         }
     }
