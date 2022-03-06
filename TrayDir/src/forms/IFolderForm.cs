30c3afef1dd6b2d5319c5a9a3044fefb1a61ce05
SamuelSVD <svd_muelo1000@hotmail.com>
2022-03-06 13:36:44 -0500

    Added File/Folder Forms

---
+++ TrayDir/src/forms/IFolderForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrayDir
+{
+    public partial class IFolderForm : Form
+    {
+        TrayInstancePath tip;
+        public IFolderForm(TrayInstancePath tip)
+        {
+            InitializeComponent();
+            this.tip = tip;
+        }
+        private void browse()
+        {
+/*            FolderSelectDialog fs = new FolderSelectDialog();
+            string path = tip.path;
+            if (path == null || path == "") {
+                fs.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            } else {
+                fs.InitialDirectory = Path.GetDirectoryName(path);
+            }
+            if (fs.ShowDialog()) {
+                instance.paths[selectedNode.tin.id].path = fs.FileName;
+                itn.Refresh();
+                MainForm.form.BuildExploreDropdown();
+                Save();
+            } */
+        }
+    }
+}
--- TrayDir/src/forms/ITreeViewForm.cs
+++ TrayDir/src/forms/ITreeViewForm.cs
@@ -223,46 +223,20 @@ namespace TrayDir
         private void docPropertiesButton_Click(object sender, EventArgs e)
         {
             ITreeNode itn = selectedNode;
-            MainForm.form.fd.DereferenceLinks = false;
-            string path = instance.paths[itn.tin.id].path;
-            if (path == null || path == "")
-            {
-                MainForm.form.fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            }
-            else
-            {
-                MainForm.form.fd.InitialDirectory = Path.GetDirectoryName(path);
-            }
-            MainForm.form.fd.FileName = Path.GetFileName(path);
-            DialogResult d = MainForm.form.fd.ShowDialog();
-            if (d == DialogResult.OK)
-            {
-                instance.paths[itn.tin.id].path = MainForm.form.fd.FileName;
-                selectedNode.Refresh();
-                MainForm.form.BuildExploreDropdown();
-                Save();
-            }
+            IFileForm iff = new IFileForm(instance.paths[itn.tin.id]);
+            iff.ShowDialog();
+            itn.Refresh();
+            itn.tin.instance.view.tray.Rebuild();
+            Save();
         }
         private void folderPropertiesButton_Click(object sender, EventArgs e)
         {
             ITreeNode itn = selectedNode;
-            FolderSelectDialog fs = new FolderSelectDialog();
-            string path = instance.paths[itn.tin.id].path;
-            if (path == null || path == "")
-            {
-                fs.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            }
-            else
-            {
-                fs.InitialDirectory = Path.GetDirectoryName(path);
-            }
-            if (fs.ShowDialog())
-            {
-                instance.paths[selectedNode.tin.id].path = fs.FileName;
-                itn.Refresh();
-                MainForm.form.BuildExploreDropdown();
-                Save();
-            }
+            IFolderForm iff = new IFolderForm(instance.paths[itn.tin.id]);
+            iff.ShowDialog();
+            itn.Refresh();
+            itn.tin.instance.view.tray.Rebuild();
+            Save();
         }
         private void UpdateButtonEnables()
         {
