672c080cda5d4902cf8571db63c3a94ef3f3e6b2
SamuelSVD <sam.svd95@gmail.com>
2022-09-21 16:47:30 -0400

    Added more assembly information

--- TrayDir/Properties/AssemblyInfo.cs
+++ TrayDir/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TrayDir")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An application to have quick access to files, folders, and programs directly from the system tray.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("samver.ca")]
 [assembly: AssemblyProduct("TrayDir")]
 [assembly: AssemblyCopyright("Copyright © samver 2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("TrayDir")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
